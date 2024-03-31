7bb2b7f759ad1f8e43edd4dcc0486d7989e33534
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-31 11:45:21 +0600

    project setup completed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Container from "../Shared/Container/Container";
+
+const Home = () => {
+    return (
+        <Container>
+            <h2>This is Home</h2>
+        </Container>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Container/Container.jsx
@@ -0,0 +1,8 @@
+
+const Container = ({children}) => {
+    return (
+        <div className='mx-auto w-full px-4 lg:max-w-screen-xl'>{children}</div>
+    );
+};
+
+export default Container;
